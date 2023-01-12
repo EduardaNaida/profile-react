bdc1cc42060b66d3c8edd2c5e792cc0c29bd206c
EduardaNaida <eduarda.naida@gmail.com>
2023-01-12 11:57:36 +0300

    feat: changed footer part

--- profile-react/src/footer/Footer.js
+++ profile-react/src/footer/Footer.js
@@ -3,24 +3,43 @@ import style from './Footer.module.scss'
 import styleC from './../common/styles/Container.module.css';
 import Items from "./Items";
 import Title from "../common/components/Title";
-import instagramIcon from "../assets/image/github-svgrepo-com.svg";
+import instagramIcon from "../assets/image/instagram.svg";
+import githubIcon from "../assets/image/github.svg";
+import linkedinIcon from "../assets/image/linkedin.svg";
+import telegramIcon from "../assets/image/telegram.svg";
+import copyRightIcon from "../assets/image/copyright.svg";
 
 const Footer = () => {
     const instagram = {
         backgroundImage: `url(${instagramIcon})`
     }
+    const github = {
+        backgroundImage: `url(${githubIcon})`
+    }
+    const linkedin = {
+        backgroundImage: `url(${linkedinIcon})`
+    }
+    const telegram = {
+        backgroundImage: `url(${telegramIcon})`
+    }
+    const copyRight = {
+        backgroundImage: `url(${copyRightIcon})`
+    }
     return (
         <div className={style.footerBlock}>
             <div className={`${styleC.container} ${style.footerContainer}`}>
                 {/*<h1 className={style.text}>Eduarda Milinkevich</h1>*/}
-                <Title className={style.title} title={'Eduarda Milinkevich'}/>
+                <h1 className={style.title}>Eduarda Milinkevich</h1>
                 <div className={style.items}>
-                    <Items style={instagram} item={'Facebook'}/>
+                    <Items style={telegram} item={'Telegram'}/>
                     <Items style={instagram} item={'Instagram'}/>
-                    <Items style={instagram} item={'Linkedin'}/>
-                    <Items style={instagram} item={'Github'}/>
+                    <Items style={linkedin} item={'Linkedin'}/>
+                    <Items style={github} item={'Github'}/>
                 </div>
-                <h3 className={style.text}>2022 All rights reserved</h3>
+                <div className={style.rights}>
+                    <div style={copyRight} className={style.icon}></div>
+                    <a className={style.text}>2022 All rights reserved</a>
+                </div>
             </div>
         </div>
     );
--- profile-react/src/footer/Items.js
+++ profile-react/src/footer/Items.js
@@ -4,8 +4,8 @@ import style from './Items.module.scss'
 const Items = (props) => {
     return (
         <div className={style.itemsBlock}>
-            <div style={props.style}></div>
-            <span className={style.items}>{props.item}</span>
+            <div className={style.icon} style={props.style}></div>
+            <span className={style.text}>{props.item}</span>
         </div>
     );
 };
